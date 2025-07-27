4e83c0d0bfe12273563d2139e8c8de2f73209f6a
Meme Supplier <owen.debiasio@gmail.com>
2025-07-27 19:53:45 -0400

    Update remove.rs

--- src/tools/remove.rs
+++ src/tools/remove.rs
@@ -3,9 +3,18 @@ use crate::tools::utils::{force_local_path, run_shell_command};
 pub fn remove() {
     let args = crate::return_args();
 
-    if let (Some(name), Some(exe_arg)) = (args.get(1), args.get(2)) {
-        let exe = force_local_path(exe_arg);
+    if let Some(name) = args.get(1) {
+        let exe = force_local_path(name);
         let exe_str = exe.to_str().expect("Invalid UTF-8 in path");
+
+        // Ask for confirmation
+        println!("Are you sure you want to delete command \"{name}\"? (y/N)");
+        let mut confirm = String::new();
+        std::io::stdin().read_line(&mut confirm).unwrap();
+        if confirm.trim().to_lowercase() != "y" {
+            println!("Aborted.");
+            return;
+        }
 
         // Remove the script file
         run_shell_command(&format!("sudo rm -f {exe_str}"));
@@ -13,7 +22,7 @@ pub fn remove() {
         // Remove the symlink
         run_shell_command(&format!("sudo rm -f {name}"));
 
-        println!("Removed {exe_str} and symlink {name}");
+        println!("\nRemoved command \"{name}\"");
     } else {
         crate::display_usage();
     }
