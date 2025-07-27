74643f4b4f55a58c2cfdc7206820440da77438de
Meme Supplier <owen.debiasio@gmail.com>
2025-07-27 19:52:46 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@ fn return_args() -> Vec<String> {
 
 fn display_usage() {
     let lines: [&str; 7] = [
-        "cmdcreate v0.2",
+        "cmdcreate v0.3",
         "",
         "Usage:",
         "",
-        "create <name> <file> <contents>",
-        "remove <name> <file>",
+        "create <name> <contents>",
+        "remove <name>",
         "list",
     ];
 
@@ -32,9 +32,12 @@ fn main() {
     }
 
     match args[0].as_str() {
-        "create" => create::create(),
-        "remove" => remove::remove(),
-        "list" => list::list(),
-        _ => display_usage(),
+        "create" if args.len() >= 2 => create::create(),
+        "remove" if args.len() >= 1 => remove::remove(),
+        "list" if args.len() == 1 => list::list(),
+        _ => {
+            eprintln!("Unknown command: '{}'", args[0]);
+            display_usage();
+        }
     }
 }
