d84dc54faeb2807d122161b64a78508345b8b7bd
Meme Supplier <owen.debiasio@gmail.com>
2025-07-28 17:35:02 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
 use std::env;
 
 mod tools;
-use tools::{create, list, remove};
+use tools::{create, edit, list, remove};
+
+static PROJ_VER: &str = "v0.4.0";
 
 fn return_args() -> Vec<String> {
     env::args().skip(1).collect()
 }
 
 fn display_usage() {
-    let lines: [&str; 7] = [
-        "cmdcreate v0.3",
+    let lines: [&str; 13] = [
+        &format!("cmdcreate {PROJ_VER}",),
         "",
         "Usage:",
         "",
-        "create <name> <contents>",
-        "remove <name>",
-        "list",
+        "create <name> <contents>    Create a custom command",
+        "remove <name>               Remove a custom command",
+        "edit <name>                 Modify a custom command",
+        "list                        Display custom commands",
+        "",
+        "Flags:",
+        "",
+        "--version                   Displays cmdcreate's version",
+        "--supported_editors         Displays supported text editors",
     ];
 
     for line in lines {
@@ -32,9 +40,22 @@ fn main() {
     }
 
     match args[0].as_str() {
+        // Normal commands
         "create" if args.len() >= 2 => create::create(),
         "remove" if args.len() >= 1 => remove::remove(),
+        "edit" if args.len() >= 1 => edit::edit(),
         "list" if args.len() == 1 => list::list(),
+
+        // Flags
+        "--hepl" if args.len() == 1 => display_usage(),
+        "--version" if args.len() == 1 => println!("{PROJ_VER}"),
+        "--supported_editors" if args.len() == 1 => {
+            println!("\nCurrent supported editors:\n");
+            for option in edit::SUPPORTED_EDITORS {
+                println!("{option}")
+            }
+        }
+
         _ => {
             eprintln!("Unknown command: '{}'", args[0]);
             display_usage();
