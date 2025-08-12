43f464cf7b5aff6cad24a7e92ec714e061dff1ae
Meme Supplier <owen.debiasio@gmail.com>
2025-08-12 22:48:52 +0000

    Update reset.rs

--- src/tools/reset.rs
+++ src/tools/reset.rs
@@ -2,7 +2,9 @@ use crate::tools::utils::*;
 
 pub fn reset() {
     // Ask for confirmation
-    println!("Are you sure you want remove ALL installed custom commands?\nTHIS CAN NOT BE UNDONE (y/N)");
+    println!(
+        "Are you sure you want remove ALL installed custom commands?\nTHIS CAN NOT BE UNDONE (y/N)"
+    );
     let mut confirm = String::new();
     std::io::stdin().read_line(&mut confirm).unwrap();
     if confirm.trim().to_lowercase() != "y" {
@@ -10,5 +12,7 @@ pub fn reset() {
         return;
     }
 
-    run_shell_command("rm -f ~/.local/share/cmdcreate/files/*");
+    run_shell_command("rm -f ~/.local/share/cmdcreate/files/*", || {
+        println!("Error: Unable to retrieve license.");
+    });
 }
