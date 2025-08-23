c682f40c3f1d1c04f0012dcb715c106190ec1e3d
Meme Supplier <owen.debiasio@gmail.com>
2025-08-23 10:40:44 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,35 @@
 mod tools;
 use tools::*;
 
-use crate::tools::utils::run_shell_command;
+use crate::tools::utils::{args_contains, run_shell_command};
 
-static PROJ_VER: &str = "v0.5.0";
+static PROJ_VER: &str = "v0.5.1";
 
 fn display_usage() {
-    let lines: [&str; 28] = [
+    let lines: [&str; 25] = [
         &format!("cmdcreate {PROJ_VER}"),
-        "",
-        "Commands:",
+        "\nCommands:",
         "  create <command> <contents>    Create a custom command",
         "  remove <command>               Remove a custom command",
         "  edit   <command>               Modify a custom command",
         "  list                           Display installed commands",
         "  search <command>               Searches for matched command",
         "  reset                          Removes all installed commands",
-        "",
-        "Arguments:",
+        "\nArguments:",
+        "  --credits                      Displays credits for cmdcreate",
         "  --version                      Displays version",
         "  --supported_editors            Displays supported text editors",
         "  --changelog                    Displays changelog",
         "  --license                      Displays license",
-        "",
-        "Offline:",
+        "  --debugging                    Displays flags used for debugging",
+        "\nOffline:",
         "  --get_offline_files            Downloads files for offline use",
         "  --remove_offline_files         Removes files for offline use",
-        "",
-        "About:",
+        "\nAbout:",
         "   Cmdcreate allows you to create custom commands for your Linux Terminal",
         "   without needing to enter the same \"complex\" commands over and over",
         "   (unless if your are lazy like me).",
-        "",
-        "   Cmdcreate will usually get smaller, more frequent updates than larger,",
+        "\n   Cmdcreate will usually get smaller, more frequent updates than larger,",
         "   less frequent updates. You should update Cmdcreate often to stay up to",
         "   date.",
     ];
@@ -50,9 +47,9 @@ fn main() {
         return;
     }
 
-    // For debugging (place as the last argument in your input)
-    if args[args.len() - 1] == "--arg_count" {
-        println!("DEBUG: ARG COUNT: {}\n", args.len())
+    // For debugging
+    if args_contains("--arg_count") {
+        println!("DEBUG: ARG COUNT: {}\n", args.len() - 1); // Subtracts 1 to exclude debug flag
     }
 
     match args[0].as_str() {
@@ -66,6 +63,7 @@ fn main() {
 
         // Arguments
         "--version" => println!("{PROJ_VER}"),
+        "--credits" => println!("cmdcreate: Credits:\n\nCreator/Maintainer:   Meme Supplier"),
         "--supported_editors" => {
             println!("\nCurrent supported editors:\n");
             for option in edit::SUPPORTED_EDITORS {
@@ -117,6 +115,18 @@ fn main() {
                 });
             },
         ),
+
+        "--debugging" => {
+            let lines: [&str; 3] = [
+                "cmdcreate: Methods for debugging:",
+                "  --arg_count             Displays number of arguments supplied",
+                "  --force_system_shell    Forces system shell to be used when running commands",
+            ];
+
+            for line in lines {
+                println!("{line}")
+            }
+        }
 
         _ => println!("cmdcreate: Invalid command entered."),
     }
