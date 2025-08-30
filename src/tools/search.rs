6ed5fddfa432edb50eda3c7fee55b30e39623f05
Meme Supplier <owen.debiasio@gmail.com>
2025-08-30 18:32:05 +0000

    Add files via upload

---
+++ src/tools/search.rs
@@ -0,0 +1,37 @@
+use crate::tools::utils::*;
+
+pub fn search() {
+    let args = return_args();
+    let mut count: i32 = 0;
+
+    if args.len() < 2 {
+        println!("Usage:\ncmdcreate search <command>");
+        return;
+    }
+
+    if let Some(name) = args.get(1) {
+        let installed_scripts = retrieve_commands("installed");
+
+        if installed_scripts.is_empty() {
+            return;
+        }
+
+        for script in installed_scripts {
+            let file_stem = script.file_stem().unwrap_or_default().to_string_lossy();
+
+            if file_stem.contains(name) {
+                println!("{file_stem}");
+                count += 1;
+            }
+        }
+
+        if count == 0 {
+            println!("No installed commands contain: {name}");
+            return;
+        }
+
+        println!("--------\nFound {count} command(s) that contain \"{name}\"")
+    } else {
+        crate::display_usage();
+    }
+}
