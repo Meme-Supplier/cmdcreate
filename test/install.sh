ac51d68fd54f05e52a5204227c67a9ec011d179f
Meme Supplier <owen.debiasio@gmail.com>
2025-10-01 16:25:30 -0400

    Update install.sh

--- test/install.sh
+++ test/install.sh
@@ -1,5 +1,7 @@
+yay -Rns cmdcreate --noconfirm || clear; echo -e "\nRelease ver not installed, skipping\n"
 sudo rm -f /usr/bin/cmdcreate
 cargo update
 rustup update
+./format.sh
 cargo build --release
 sudo cp ./target/release/cmdcreate /usr/bin
